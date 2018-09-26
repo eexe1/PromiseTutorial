7f68eedb351328cbd8fa722cd9cb5e8c4aed6e1c
Evan <devillordking@gmail.com>
2018-09-26 13:13:29 +1000

    Initial Commit

---
+++ PromiseTutorial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PromiseTutorial
+//
+//  Created by Evan Tsai on 26/9/18.
+//  Copyright © 2018 Evan Tsai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
